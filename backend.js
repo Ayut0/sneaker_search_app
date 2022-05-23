99a0df99fceb614d8c6e7f904de827fbed3827de
Yuto Yamakita <yuvto.0420@gmail.com>
2022-05-22 20:54:36 -0700

    Initial commit

---
+++ backend.js
@@ -0,0 +1,34 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json("hi");
+});
+
+app.get("/sneaker-data", (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://the-sneaker-database.p.rapidapi.com/search",
+    params: { limit: "10", query: "airjordan" },
+    headers: {
+      "X-RapidAPI-Host": "the-sneaker-database.p.rapidapi.com",
+      "X-RapidAPI-Key": `${process.env.REACT_APP_RAPID_API_KEY}`,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+app.listen(8000, () => console.log(`Server is running on ${PORT}`));
---
+++ src/GetSneakerData/GetSneakerData.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://the-sneaker-database.p.rapidapi.com/search",
+  params: { limit: "10", query: "airjordan" },
+  headers: {
+    "X-RapidAPI-Host": "the-sneaker-database.p.rapidapi.com",
+    "X-RapidAPI-Key": `${process.env.REACT_APP_RAPID_API_KEY}`,
+  },
+};
+
+const GetSneaker = () => {
+  const [sneakerData, setSneakerData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setSneakerData(response.data);
+        console.log(sneakerData);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+};
+
+export default GetSneaker;
