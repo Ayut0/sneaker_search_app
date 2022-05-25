be9667ce87c722dff11b80db9835d973938cc6e4
Yuto Yamakita <yuvto.0420@gmail.com>
2022-05-24 20:24:53 -0700

    Finish setting nonexistent page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import HeroPage from './HeroPage/HeroPage';
 import Home from './Home/Home';
 import Detail from './DetailPage/DetailPage';
+import NotFound from './NotFoundPage/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello React Router v6</h1>
+        {/* <h1>Hello React Router v6</h1> */}
         <Routes>
           <Route path='/hero' element={<HeroPage />}/>
           <Route path='/' element={<Home />}/>
           <Route path='/detail' element={<Detail />}/>
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </header>
     </div>
---
+++ src/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,5 @@
+const NotFound = () =>{
+    return <h2>Page is not found...</h2>
+}
+
+export default NotFound;
